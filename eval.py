ddb45d8d6c38105c2092cf8616edaf6179867985
kai shun <kai.teoh85@gmail.com>
2025-11-13 23:18:10 +0800

    req updates + eval code updates

--- eval.py
+++ eval.py
@@ -2,6 +2,22 @@ import cv2
 import torch 
 import numpy as np 
 
+# SRGAN evaluation function
+def load_checkpoint_SRGAN(generator, discriminator, optimizer_G, optimizer_D, filename,type):
+    checkpoint = torch.load(filename)
+    generator.load_state_dict(checkpoint['generator_state_dict'])
+    discriminator.load_state_dict(checkpoint['discriminator_state_dict'])
+    optimizer_G.load_state_dict(checkpoint['optimizer_G_state_dict'])
+    optimizer_D.load_state_dict(checkpoint['optimizer_D_state_dict'])
+    epoch = checkpoint['epoch']
+    os.makedirs('model_checkpoints', exist_ok=True)
+    filename = os.path.join('model_checkpoints', 'SRGAN', filename)
+    print(f'Checkpoint loaded: {filename} (epoch {epoch})')
+    return epoch
+
+
+
+# SRCNN evaluation function
 def eval_SRCNN(model, image_path, scale_factor=3, device='cuda'):
     """
     Super-resolve a single image
@@ -38,3 +54,24 @@ def eval_SRCNN(model, image_path, scale_factor=3, device='cuda'):
     img_sr = cv2.cvtColor(img_ycrcb, cv2.COLOR_YCrCb2BGR)
     
     return img_sr, y_bicubic, y_sr
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Script to allow choosing of model type")
+    parser.add_argument("--model", help="the model you want to train")
+
+    args = parser.parse_args()
+    if args.model == 'SRCNN':
+        from src_SRCNN import SRCNN
+        model = SRCNN()
+        model.load_state_dict(torch.load('model_checkpoints/SRCNN/checkpoint.pth'))
+    else:
+        from src_SRGAN import SRGAN
+        
+        generator = SRGAN()
+        discriminator = SRGAN()
+        optimizer_G = torch.optim.Adam(generator.parameters(), lr=1e-4, betas=(0.9, 0.999))
+        optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=1e-4, betas=(0.9, 0.999))
+        epoch = load_checkpoint_SRGAN(generator, discriminator, optimizer_G, optimizer_D, 'checkpoint_epoch_100.pth', args.model)
+
+        
